d8e62cf409bc7f2b45addf37af84d474f1f974ac
Anthony Ebiner <anthonyebiner@gmail.com>
2020-10-23 00:19:58 -0700

    Initial commit

---
+++ desktop/src/com/bears/drift/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bears.drift.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bears.drift.Drift;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drift(), config);
+	}
+}
---
+++ html/src/com/bears/drift/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.bears.drift.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.bears.drift.Drift;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drift();
+        }
+}
