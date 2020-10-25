1e724f55c69b8025bde3c9181f7cc87c89859748
Anthony Ebiner <anthonyebiner@gmail.com>
2020-10-24 20:57:35 -0700

    add getTexture

--- core/src/com/bears/drift/Drift.java
+++ core/src/com/bears/drift/Drift.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.HashMap;
+
 public class Drift extends Game {
 	SpriteBatch batch;
 	Texture img;
@@ -12,14 +14,26 @@ public class Drift extends Game {
 	int screenSizeY;
 	int centerX;
 	int centerY;
+	private HashMap<String, Texture> loaded;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		loaded = new HashMap<>();
 		screenSizeX = 1680; screenSizeY = 1680;
 		centerX = screenSizeX/2; centerY = screenSizeY/2;
 		Gdx.graphics.setWindowedMode(screenSizeX, screenSizeY);
 		this.setScreen(new GameScreen(this));
+	}
+
+	public Texture getTexture(String path) {
+		if (loaded.containsKey(path)) {
+			return loaded.get(path);
+		} else {
+			Texture texture = new Texture(path);
+			loaded.put(path, texture);
+			return texture;
+		}
 	}
 
 	@Override
